0e56a38f5fc3b8440819ea1457b0377fcb513ebb
Astorrr101 <126696967+Astorrr101@users.noreply.github.com>
2025-05-27 14:29:45 -0600

    Add files via upload

---
+++ trabajos_clases/src/main/java/com/mycompany/trabajos_clases/Trabajos_clases.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabajos_clases;
+
+/**
+ *
+ * @author pc
+ */
+public class Trabajos_clases {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
